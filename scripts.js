e1f04a83a1ea0a1f30256fae2b59f6b4b6e09279
FuluZwivhuya <148289949+FuluZwivhuya@users.noreply.github.com>
2024-01-25 20:47:20 +0200

    yet to amend challenge 3

---
+++ scripts.js
@@ -0,0 +1,52 @@
+console.log ("happy"); //test run
+
+//challenge 1
+
+/* This is the firstname of the user /*
+
+const user = 'John'
+
+
+/* This is the lastname of the user /* const surname = 'Smith'
+
+
+console.log(user; surname;)
+
+/*solution for challenge 1*/
+
+const user = "John";
+const surname = "Smith";
+console.log(user,surname);
+
+
+//Challange 2
+
+/** (c) ACME Inc. 2010 */
+
+/*
+ * This is the date that a user created their account 
+ 
+const date = '10/07/2014'
+
+console(date)
+
+/*solution for challenge 2*/
+
+const date = "10/7/2014";
+console.log (date);
+//This is the date that a user created their account
+
+
+//Challenge 3
+/** 
+It is important to show the following message in order to:
+* to assure users
+* to scare hackers
+* to impress investors
+
+
+console.warn(Security scan starting)
+
+/* It is important to let user know when they can close the page  
+
+console.info( Please wait for scan to complete before closing the browser.)*/
